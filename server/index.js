615dbc8ff6ca5732e9986fb939f2d206735e7c49
mihatronych <44722290+mihatronych@users.noreply.github.com>
2022-02-06 16:31:20 +0500

    Начало разворота бэка

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
